58d90b7f2ade78522e25fa6154ab8d0066ee7aac
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2019-11-21 17:33:23 +0100

    Update test (it works)

--- test/main.cpp
+++ test/main.cpp
@@ -12,14 +12,24 @@ struct Position {
 TEST_CASE( "Name of the case", "[category]" ) {
     met::Registry registry;
     Position pos;
+
+    // Entity 1
     pos.x = 2;
     pos.y = 3;
-
     auto entity = registry.create();
     registry.assign<Position>(entity, pos);
 
-    auto storedPos = registry.get<Position>(entity);
-    std::cout << storedPos.x << " " << storedPos.y << std::endl;
+    // Entity 2
+    pos.x = 6;
+    pos.y = 8;
+    auto entity2 = registry.create();
+    registry.assign<Position>(entity2, pos);
+
+    auto storedPos1 = registry.get<Position>(entity);
+    std::cout << storedPos1.x << " " << storedPos1.y << std::endl;
+
+    auto storedPos2 = registry.get<Position>(entity2);
+    std::cout << storedPos2.x << " " << storedPos2.y << std::endl;
 
     REQUIRE( true );
 }
